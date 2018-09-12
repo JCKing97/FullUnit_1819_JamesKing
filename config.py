74298c4471aa2928bab6f9a71ce644bfe59e9670
RHUL-James-King <james.king.2015@live.rhul.ac.uk>
2018-09-12 19:25:55 +0100

    Setting up for heroku deploy

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """"Contains config information for the running of the server"""
-
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
