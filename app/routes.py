55817ed5337a29e810b108002de9dcd7c82ab5e6
RHUL-James-King <james.king.2015@live.rhul.ac.uk>
2018-10-15 17:46:53 +0100

    The react component can now send the players to the server to play the tournament. Reformatted some of the match and html components to display correctly with the changes I had to make for the react component. Next step is to throw the tournament to a redis task queue and then once completed redirect them to an analysis page.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app, db
 from app.models import Match, Round, Player, Action
 from app.forms import MatchSelectPlayersForm
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash, json
 import axelrod as axl
 from app.axelrod_database_conversion import match_result_to_database
 from app.analysis import get_match_points
+import pprint
 
 @app.route('/')
 @app.route('/index')
@@ -18,24 +19,23 @@ def match(level):
     """Handles rendering the template with the form to select
     and start a match and also the logic when this form is posted"""
     if level == "Advanced":
-        strategies = [s() for s in axl.strategies]
+        strategies = [{'id': axl.strategies.index(s), 'name': s.name} for s in axl.strategies]
     else:
-        strategies = [s() for s in axl.basic_strategies]
-    print(strategies)
+        strategies = [{'id': axl.basic_strategies.index(s), 'name': s.name} for s in axl.basic_strategies]
+    strat_dict = {s.name: s() for s in axl.strategies}
     form = MatchSelectPlayersForm()
     edit_players(form, strategies)
     if form.validate_on_submit():
-        strat_dict = {strat.name: strat for strat in strategies}
         players = (strat_dict[form.strats_field1.data], strat_dict[form.strats_field2.data])
         result = axl.Match(players, turns=form.rounds.data).play()
         match_id = match_result_to_database(results=result, players=players)
         return redirect(url_for('match_run', match_id=match_id))
-    return render_template('match.html', title='Match', form=form, strategies=strategies)
+    return render_template('match.html', title='Match', form=form, strategies=strategies, strat_dict=strat_dict)
 
 def edit_players(form, strategies):
     """Creates the choices for the strategy selection fields for a player"""
-    form.strats_field1.choices = [(strat.name, strat.name) for strat in strategies]
-    form.strats_field2.choices = [(strat.name, strat.name) for strat in strategies]
+    form.strats_field1.choices = [(strat['name'], strat['name']) for strat in strategies]
+    form.strats_field2.choices = [(strat['name'], strat['name']) for strat in strategies]
 
 
 @app.route('/match_run/<match_id>')
@@ -50,17 +50,33 @@ def match_run(match_id):
                            strat_dict=strat_dict, players=players)
 
 
-@app.route('/tournament/<level>')
+@app.route('/tournament/<level>', methods=['GET', 'POST'])
 def tournament(level):
+    # Get strategies based on level
     if level == "Advanced":
-        strategies = [s() for s in axl.strategies]
+        strategies = [{'id': axl.strategies.index(s), 'name': s.name} for s in axl.strategies]
     else:
-        strategies = [s() for s in axl.basic_strategies]
-    return render_template('tournament.html', title='Tournament', strategies=strategies)
+        strategies = [{'id': axl.basic_strategies.index(s), 'name': s.name} for s in axl.basic_strategies]
+    strat_dict = {s().name: s() for s in axl.strategies}
+    if request.method == 'GET':
+        return render_template('tournament.html', level=level, title='Tournament', strategies=strategies, strat_dict=strat_dict)
+    if request.method == 'POST':
+        strategy_counts = request.get_json()['strategy_counts']
+        players = []
+        for strategy in strategy_counts:
+            for i in range(0, strategy['count']):
+                players.append(strat_dict[strategy['name']])
+        if 2 < len(players) < 50:
+            results = axl.Tournament(players).play()
+            pprint.pprint(results.summarise())
+            return redirect(url_for('tournament_run', tournament_id=tournament_id))
+        else:
+            flash("Must have selected between 3 and 50 players for the tournament")
+            return render_template('tournament.html', title='Tournament', strategies=strategies, strat_dict=strat_dict)
 
 
-@app.route('/tournament_finished/<tournament_id>')
-def tournament_finished(tournament_id):
+@app.route('/tournament_run/<tournament_id>')
+def tournament_run(tournament_id):
     """Displays the information of a finished tournament with the tournament_id provided"""
     matches = Tournament.query.filter_by(id=tournament_id).first_or_404().matches.all()
     player_points = get_tournament_points(matches)
